adc839c2a9aab589741360c4d963793a1e7ce463
Icaro-pavani <icaro.pavani79@gmail.com>
2022-05-08 10:39:28 -0300

    added the stripHtlm tool

--- src/middlewares/validLoginBody.js
+++ src/middlewares/validLoginBody.js
@@ -1,8 +1,14 @@
 import loginSchema from "../schemas/loginSchema.js";
+import { stripHtml } from "string-strip-html";
 
 export default async function validLoginBody(req, res, next) {
+  const { email, password } = req.body;
+  const loginBody = {
+    email: stripHtml(email).result.trim(),
+    password: stripHtml(password).result.trim(),
+  };
   try {
-    const loginValidation = await loginSchema.validateAsync(req.body);
+    const loginValidation = await loginSchema.validateAsync(loginBody);
     res.locals.body = loginValidation;
   } catch (error) {
     console.log(error);
