5604547999d40efbfd66534552496281a950e076
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-19 16:05:22 -0300

    Preparando revisão de código

--- src/schemas/bodySchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const bodySchema = joi.object({
-  description: joi.string().required(),
-  value: joi.number().required(),
-  type: joi.string().valid("credit", "debit").required(),
-});
-
-export default bodySchema;
